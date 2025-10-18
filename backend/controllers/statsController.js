478d5ef126ee674990ade38aa752be9012cd9d02
hain <hainofficialweb@gmail.com>
2025-10-18 18:14:19 +0530

    refactor(stats): optimize getStats logic with category grouping and total calculations

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -1,15 +1,27 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
 const getStats = (req, res) => {
-  const products = Product.findByUser(req.user.id);
-  const totalProducts = products.length;
-  const totalValue = products.reduce((sum, p) => sum + p.price * p.stock, 0);
-  const categories = [...new Set(products.map(p => p.category))];
-  const productsPerCategory = categories.map(cat => ({
-    category: cat,
-    count: products.filter(p => p.category === cat).length
-  }));
-  res.json({ totalProducts, totalValue, categories: categories.length, productsPerCategory });
+  const userProducts = Product.findByUser(req.user.id);
+
+  const productsByCategory = userProducts.reduce((acc, product) => {
+    const category = product.category.toLowerCase();
+    if (!acc[category]) {
+      acc[category] = { count: 0, value: 0 };
+    }
+    acc[category].count += 1;
+    acc[category].value += product.price * product.stock;
+    return acc;
+  }, {});
+
+  const stats = {
+    totalProducts: userProducts.length,
+    totalCategories: Object.keys(productsByCategory).length,
+    totalValue: userProducts.reduce((sum, p) => sum + p.price * p.stock, 0),
+    totalStock: userProducts.reduce((sum, p) => sum + p.stock, 0),
+    productsByCategory,
+  };
+
+  res.json(stats);
 };
 
 module.exports = { getStats };
